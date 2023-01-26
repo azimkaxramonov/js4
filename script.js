ff4f12bb4a44dfbd61596ff00d151aedcea053b2
SnusPower <121939990+azimkaxramonov@users.noreply.github.com>
2023-01-26 11:52:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let username = prompt("как вас зовут?").toLowerCase().trim()
+
+if(
+    username === 'alex' ){
+        console.log (" даро укажон " +  username)
+    let account = +prompt('Номер счёта?')
+    if(
+        account === 7777){
+      alert(' на балансе 10000 ')
+      console.log(' даро укажон ' + username)
+      let money = +prompt(' сколько обналичить? ')
+    
+if(
+money >= 10000 <=0
+){
+    let balance = 10000
+    let totalbalance = balance-money
+   alert(' подождите ' )
+   alert(' вы сняли ' + money)
+   alert( 'осталось ' + totalbalance)
+
+    }
+} else{
+    console.log (' кан анжа ' + username)
+}
+} else {
+    console.log (" кан анжа братиш " +  username)
+    } 
+
+
+
